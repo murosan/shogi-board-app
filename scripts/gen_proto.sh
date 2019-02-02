354f93e85f935cb217153c3c00ba9e813689bb36
murosan <m.murota.dev@gmail.com>
2019-02-03 01:28:33 +0900

    :+1: create-react-app では出力した js に eslint-disable を入れる必要があった

--- scripts/gen_proto.sh
+++ scripts/gen_proto.sh
@@ -10,6 +10,7 @@ echo "app_dir:" $app_dir
 
 proto_dir="$app_dir/shogi-board-protobufs/protos"
 proto_file="v1.proto"
+js_out_proto_file="v1_pb.js"
 
 server_out="$app_dir/shogi-board-server/app/proto"
 front_out="$app_dir/shogi-board/src/proto"
@@ -24,5 +25,12 @@ protoc \
   --go_out=plugins=grpc:$server_out \
   --proto_path=$proto_dir \
   $proto_file
+
+# create react app では、eslint に引っかかるとコンパイルに失敗するので
+# 生成された js ファイルに eslint-disable を入れる
+# .eslintignore は意味なかった
+js_target="$front_out/$js_out_proto_file"
+echo "adding eslint-disable to $js_target"
+sed -i '' '1s!^!/* eslint-disable */!' $js_target
 
 echo "done ✨"
